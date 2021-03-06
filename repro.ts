a8c1f33a4d76faeb872592e85104791b44067b77
pbalaga <ppbalaga@gmail.com>
2021-03-06 08:22:44 +0100

    minimal repro steps

---
+++ repro.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata";
+import { Container, inject, injectable, optional } from 'inversify';
+
+export type Constructor<T = {}> = new (...args: any[]) => T;
+
+type ComponentConfig = {
+    someAttribute: 'option 1' | 'option 2';
+}
+
+@injectable()
+class Dependency {
+
+}
+
+@injectable()
+class ComponentWithDefault {
+    constructor(
+        public dependency: Dependency,
+        @inject('identifier') @optional() public config: ComponentConfig = { someAttribute: 'option 2' }) {
+    }
+}
+
+@injectable()
+class ComponentWithoutDefault {
+    constructor(
+        public dependency: Dependency,
+        @inject('identifier') @optional() public config: ComponentConfig) {
+    }
+}
+
+const container = new Container({ skipBaseClassChecks: true });
+container.bind(ComponentWithDefault).toSelf();
+container.bind(ComponentWithoutDefault).toSelf();
+container.bind(Dependency).toSelf();
+
+const childContainer = new Container({ skipBaseClassChecks: true });
+childContainer.parent = container;
+childContainer.bind('identifier').toConstantValue(<ComponentConfig>{
+    someAttribute: 'option 1'
+});
+
+console.log('with default', childContainer.get(ComponentWithDefault).config);
+console.log('without default', childContainer.get(ComponentWithoutDefault).config);
+
+console.log(ComponentWithDefault.length);
+console.log(ComponentWithoutDefault.length);
